99f8ee362c32115dd77bfb9254aadd02b9195eb7
TharHtet236401 <tharhteta3@gmail.com>
2024-07-09 02:14:29 +0700

    after adding typing status and fix some bugs

--- index.js
+++ index.js
@@ -13,37 +13,32 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('User connected');
+  
+  let nickname = 'Anonymous';
 
-  // // Broadcast to all clients that a new user has connected
-  // io.emit('chat message', 'User connected');
+  socket.on('set nickname', (name) => {
+    nickname = name;
+    socket.broadcast.emit('chat message', `${nickname} connected`);
+    console.log(`${nickname} connected`);
+  });
 
-  // Broadcast to all clients except the sender;
-  socket.broadcast.emit("chat message");
-
-  // Listening for 'chat message' events from clients
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-    // Broadcast the received message to all connected clients
-    io.emit('chat message', msg);
+    io.emit('chat message', `${nickname}: ${msg}`);
   });
 
-  // Handle user disconnection
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', nickname);
+  });
+
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing');
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
-    // Broadcast to all clients that a user has disconnected
     io.emit('chat message', `${nickname} disconnected`);
   });
-
-    // Temporarily store the nickname
-    let nickname = 'Anonymous';
-
-    // Listen for 'set nickname' event to get the user's nickname
-    socket.on('set nickname', (name) => {
-      nickname = name;
-      socket.broadcast.emit('chat message', `${nickname} connected`);
-      console.log(nickname);
-    });
-
 });
 
 server.listen(3000, () => {

d31750dda1b3dab8890e4c0b8c9ed43be349b019
TharHtet236401 <tharhteta3@gmail.com>
2024-07-11 01:34:53 +0700

    after integrating with mongo atlas online

--- db/db.js
+++ db/db.js
@@ -1,11 +1,16 @@
-// db.js
+require('dotenv').config(); 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/chat', {  });
+
+const url = process.env.MONGODB_URI; 
+
+mongoose.connect(url)
+    .then(() => console.log('MongoDB Atlas connected'))
+    .catch(err => console.error('MongoDB Atlas connection error:', err));
 
 const messageSchema = new mongoose.Schema({
     user: String,
     message: String,
-    image: String, 
+    image: String,
     timestamp: { type: Date, default: Date.now }
 });
 
